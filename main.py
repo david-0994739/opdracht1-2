84e5303f66464cdb0d0dfbc2d3daeee734bcb933
david-0994739 <113016353+david-0994739@users.noreply.github.com>
2022-09-07 13:19:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import math
+
+# Opdracht 1
+
+# print(23.95 * 32.9)
+# print(23.95 * 32.9 + 6)
+# print(23.95 * 32.9 + 6)
+# print(23.95 * 32.9 + 6)
+# print(23.95 / 32.9)
+# print(23.95 / 32.9 + 6)
+# print((23.95 / 32.9) + 6)
+# print(23.95 / 32.9 + 6)
+
+
+# student = 28
+# teachers = 3
+# seats = student + teachers
+# correct = student > teachers
+# incorrect = student < teachers
+# print(incorrect)
+#
+# line_character = '-'
+# line = line_character * 20
+# print(line)
+
+# city = 'Rotterdam'
+# print(city)
+# type(city)
+# residents = 500
+# print(residents)
+# type(residents)
+# area = 324.1
+# print(area)
+# type(area)
+# print(f'The city of {city}')
+
+
+
+# Opdracht 2
+
+# Berekening1
+y = math.sqrt(3*9.1-1) + (1+9.1)**2
+print(y)
+
+# Berekening2
+x = -2.7 + math.sqrt(2.7**2)-(4*0.87*5.03) / 2*0.87
+print(x)
+
+# Berekening3
+delta = 4 * (1/(179875478.8*(1-179875478.8**2/299792458**2)))
+print(delta)
